40b9007efafd8dc02a4abad695b181eeda60c128
pete-rjames <peter.riessjames@gmail.com>
2016-08-08 21:56:30 -0700

    Create acs.R

---
+++ acs.R
@@ -0,0 +1,3 @@
+# ACS
+# http://eglenn.scripts.mit.edu/citystate/2016/03/acs-version-2-0-now-on-cran
+# http://dusp.mit.edu/sites/dusp.mit.edu/files/attachments/publications/working_with_acs_R_v_2.0.pdf
